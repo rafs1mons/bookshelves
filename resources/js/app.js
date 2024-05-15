de317e48908fe316c4b4476f700df8687143ef45
root <aakkaya300@gmail.com>
2024-05-15 14:15:32 +0300

    npm packs update
    Signed-off-by: root <aakkaya300@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,9 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
+import jQuery from 'jquery';
+window.$ = jQuery;
+
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
  * registering components with the application instance so they are ready
